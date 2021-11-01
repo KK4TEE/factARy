33622334b16c57360b33775a5b492c9d14820a05
Seth Persigehl <KK4TEE@users.noreply.github.com>
2021-10-31 20:45:27 -0700

    Automatic windows log path
    Python cleanup
    Extra home screen status output
    Error status in response

--- factorio_mod/server.py
+++ factorio_mod/server.py
@@ -12,55 +12,48 @@ import json
 import os
 import time
 
-operatingSystem = 'Windows'
-osUserName = 'YOUR_OS_USERNAME_HERE'
 
 lastTime = 0
 interval = 0.033
-## CHANGE THE BELOW PATHS TO POINT TO YOUR FACTORIO DIRECTORY ##
 
-if (operatingSystem is 'Windows'):
-    filepath_before_username = 'C:\\Users\\'
-    filepath_after_username = '\\AppData\\Roaming\\Factorio\\script-output\\'
+if os.name == 'nt':  # 'nt' is the code for Windows
+    os.getenv('username')
+    outputPath = os.path.join(os.path.expandvars("%userprofile%"), "AppData", "Roaming", "Factorio", "script-output")
     logFileName = 'factARy_log'
     logFileExtension = '.json'
-    filepath1 = filepath_before_username + \
-                osUserName + \
-                filepath_after_username + \
-                logFileName + '1' + logFileExtension
-    filepath2 = filepath_before_username + \
-                osUserName + \
-                filepath_after_username + \
-                logFileName + '2' + logFileExtension
+    filepath1 = os.path.join(outputPath, logFileName + '1' + logFileExtension)
+    filepath2 = os.path.join(outputPath, logFileName + '2' + logFileExtension)
 else:
+    # If you are not on Windows, you can manually specify the path to the json files here
     filepath1 = "OS_NOT_SUPPORTED"
     filepath2 = "OS_NOT_SUPPORTED"
 
-# In Windows 10, the filepaths should look something like this:    
+# In Windows 10, the filepaths should look something like this:
 # filepath1 = r'C:\Users\USERNAME\AppData\Roaming\Factorio\script-output\factARy_log1.json'
 # filepath2 = r'C:\Users\USERNAME\AppData\Roaming\Factorio\script-output\factARy_log2.json'
 print("Filepath for logfile 1 is: '" + filepath1 + "'")
 
 data = {}
 
-def readData():
+
+def read_data():
     global filepath1
     global filepath2
     
-    if (os.path.getmtime(filepath1) > os.path.getmtime(filepath2)):
+    if os.path.getmtime(filepath1) > os.path.getmtime(filepath2):
         filepath = filepath2
         print("Using filepath 2")
     else:
         filepath = filepath1
         print("Using filepath 1")
     
-    filestring = open(filepath, "r")
-    d = json.load(filestring)
-    filestring.close()
+    file_string = open(filepath, "r")
+    d = json.load(file_string)
+    file_string.close()
     return d
 
 
-def ReadAttemptManager():
+def read_attempt_manager():
     global interval
     global lastTime
     global data
@@ -68,45 +61,63 @@ def ReadAttemptManager():
     if time.time() > lastTime + interval:
         lastTime = time.time()
         try:
-            data = readData()
+            data = read_data()
             print("timestamp path1: " +
                   str(os.path.getmtime(filepath1)) +
                   " timestamp path2: " +
                   str(os.path.getmtime(filepath2)))
             print("tick: " + data["tick"])
+            data["status"] = "ok"
+            data["error"] = ""
         except ValueError:
-            print("=== Error: Values Invalid!")
+            data["status"] = "error"
+            data["error"] = "Values Invalid"
+            print(f"=== Error: {data['error']}!")
             lastTime = time.time() + 0.01
-            data = ReadAttemptManager()
+            data = read_attempt_manager()
         except PermissionError:
-            print("=== Error: Permission Error!")
+            data["status"] = "error"
+            data["error"] = "Permission Error"
+            print(f"=== Error: {data['error']}!")
             lastTime = time.time() + 0.01
         except FileNotFoundError:
-            print("=== Error: File not found!")
+            data["status"] = "error"
+            data["error"] = "File not found"
+            print(f"=== Error: {data['error']}!")
             lastTime = time.time() + 0.01
     return data
 
-################################################################################
 
 class Root:
 
     @cherrypy.expose
     def index(self):
+        global filepath1
+        global data
+        global lastTime
+        data = read_attempt_manager()
+
         s = "<h1>FactARy Server </h1><br>"
-        s += "Client IP: "
-        s += str(cherrypy.request.remote.ip)
-        s += "<br>Server Socket: "
-        s += str(cherrypy.server.socket_host)
+        s += f"Client IP: {str(cherrypy.request.remote.ip)} <br>"
+        s += f"Server Socket: {str(cherrypy.server.socket_host)} <br>"
+        s += f"File Path: '{filepath1}' <br>"
+        s += f"Last Attempt Time: '{lastTime}' <br>"
+        s += f"Status: {data['status']} <br>"
+
+        if len(data['error']) > 0:
+            s += f"Error: {data['error']} <br>"
+        else:
+            s += "Click <a href=\"/json\">here</a> for JSON data"
+
         return s
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def json(self):
         global data
-        data = ReadAttemptManager()
+        data = read_attempt_manager()
         return data
 
-################################################################################
 
 if __name__ == "__main__":
     
